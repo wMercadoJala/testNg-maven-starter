2632e199765dd2058d7a8b261328078a0409bc6e
Walter Mercado <Walter.Mercado@jalasoft.com>
2017-08-08 09:24:51 -0400

    Update Changes

--- src/test/java/samples/SampleTest.java
+++ src/test/java/samples/SampleTest.java
@@ -7,20 +7,9 @@ import org.testng.annotations.Test;
  * Created by trierra on 7/23/15.
  */
 public class SampleTest {
-
-    @Test(groups = {"unit"})
-    public void first(){
-        System.out.println("unit");
-    }
-
     @Test(groups = {"web"})
     public void second(){
         System.out.println("web");
-        Assert.assertEquals("2", "1");
-    }
-
-    @Test(groups = {"db"})
-    public void third(){
-        System.out.println("db");
+        Assert.assertEquals("2", "2");
     }
 }
