30d128abe8defc32bfeb62d5598046e89cb3c165
Walter Mercado <Walter.Mercado@jalasoft.com>
2017-08-08 10:36:58 -0400

    Added second test

--- src/test/java/samples/SampleTest.java
+++ src/test/java/samples/SampleTest.java
@@ -6,10 +6,16 @@ import org.testng.annotations.Test;
 /**
  * Created by trierra on 7/23/15.
  */
-public class SampleTest {
-    @Test(groups = {"web"})
+    public class SampleTest {
+    @Test()
     public void second(){
         System.out.println("web");
         Assert.assertEquals("2", "2");
+    }
+
+    @Test()
+    public void first(){
+        System.out.println("web");
+        Assert.assertEquals("2", "1");
     }
 }
